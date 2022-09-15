6677dd6604d820843036f9c94efe62ef121321d9
infoengine1337 <infoengine1337@gmail.com>
2022-09-15 10:22:44 +0900

    sted

--- ease_comp.lua
+++ ease_comp.lua
@@ -10,9 +10,9 @@ comp.easing=function(num,ratio,st,ed,hoge)
 	if num > 0 then
 		local E = require("ease_common")
             if 0 < n and n <= 41 then
-                  return E.auto(x, 0, 1, -n)
+                  return E.auto(x, st, td, -n)
             else
-                  return E.auto(x, 0, 1, n)
+                  return E.auto(x, st, td, n)
             end
       end
 end

11b091edd72e2e8090d2c2a7728878927986b43c
infoengine1337 <infoengine1337@gmail.com>
2022-09-15 10:41:43 +0900

    torima

--- ease_comp.lua
+++ ease_comp.lua
@@ -3,16 +3,17 @@
 	@analogue1337
 ]]
 
-local ease_common=require "ease_common"
+local ease_common = require("ease_common")
 local comp={}
 
 comp.easing=function(num,ratio,st,ed,hoge)
 	if num > 0 then
-		local E = require("ease_common")
             if 0 < num and num <= 41 then
-                  return E.auto(ratio, st, ed, -num)
+                  return ease_common.auto(ratio, st, ed, -num)
             else
-                  return E.auto(ratio, st, ed, num)
+                  return ease_common.auto(ratio, st, ed, num)
             end
       end
 end
+
+return comp

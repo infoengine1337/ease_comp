88cdb477a62f39afdf4e2e805ffa7a01b3438c63
infoengine1337 <infoengine1337@gmail.com>
2022-09-15 10:23:42 +0900

    numratio

--- ease_comp.lua
+++ ease_comp.lua
@@ -10,9 +10,9 @@ comp.easing=function(num,ratio,st,ed,hoge)
 	if num > 0 then
 		local E = require("ease_common")
             if 0 < n and n <= 41 then
-                  return E.auto(x, st, td, -n)
+                  return E.auto(ratio, st, ed, -num)
             else
-                  return E.auto(x, st, td, n)
+                  return E.auto(ratio, st, ed, num)
             end
       end
 end
